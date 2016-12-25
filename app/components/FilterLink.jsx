2041a038561b59ffe9a2f04edf7a211ec7717427
bilal-fazlani <bilal.m.fazlani@gmail.com>
2016-12-26 00:47:22 +0530

    added badges on filter bar that show count

--- app/components/FilterLink.jsx
+++ app/components/FilterLink.jsx
@@ -1,35 +1,51 @@
 import React from "react"
 import {connect} from "react-redux"
 
-const Link = ({isActive, children, onLinkClick}) => {
+const Link = ({isActive, children, onLinkClick, count}) => {
     if(isActive === true){
         return <li role="presentation" className="active">
-            <a href="#" >{children}</a>
+            <a href="#" >{children} <span className="badge">{count}</span></a>
             </li>
 
     }
 
     return <li role="presentation">
-    <a href="#" 
+    <a href="#"
               onClick={(e)=> {
                   e.preventDefault();
                   onLinkClick();
               }
-              }>{children}</a>
+              }>{children} <span className="badge">{count}</span></a>
     </li>
 }
 
-let FilterLink = ({filter, visibilityFilter, dispatch, children}) => {
+let FilterLink = ({filter, visibilityFilter, dispatch,
+    children, count}) => {
     return <Link
             onLinkClick={()=>dispatch({type:"SET_VISIBILITY", filter})}
             isActive={filter === visibilityFilter}
+            count={count}
     >{children}</Link>
 }
 
-FilterLink = connect((state)=>{
+FilterLink = connect((state, ownProps)=>{
     return {
-        visibilityFilter: state.visibilityFilter
+        visibilityFilter: state.visibilityFilter,
+        count: getBadgeCount(ownProps.filter, state.todos)
     }
 })(FilterLink);
+
+const getBadgeCount = (filter, todos)=>{
+    switch(filter){
+        case "SHOW_ALL":
+            return todos.length
+        case "SHOW_COMPLETED":
+            return todos.filter(x=>x.completed).length
+        case "SHOW_ACTIVE":
+            return todos.filter(x=>!x.completed).length
+        default:
+            return -1;
+    }
+}
 
 export default FilterLink;
